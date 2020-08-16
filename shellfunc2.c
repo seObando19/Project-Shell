6dd3371e84bd4497a15e87718c6af159de839868
hfog2066 <hfog2000@yahoo.com>
2020-08-16 15:34:48 -0500

    created strcat function

--- shellfunc2.c
+++ shellfunc2.c
@@ -65,3 +65,28 @@ char *get_value_env(char **envp, char *variable)
 	}
 	return (NULL);
 }
+/**
+ * char *_strcat - concatenates two strings.
+ * @dest: *dest - destination
+ * @src: *src  - source
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int i2 = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[i2])
+	{
+
+		dest[i + i2] = src[i2];
+		i2++;
+	}
+	dest[i + i2] = src[i2];
+	return (dest);
+}
