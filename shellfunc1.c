08a6140bd2215b88f24ea6fb101694177b671315
seObando19 <sebastiano-10@hotmail.com>
2020-08-16 18:41:11 +0000

    Updated file with the listenread function

--- shellfunc1.c
+++ shellfunc1.c
@@ -10,3 +10,21 @@ void prompt(void)
 	printf("\x1B[1;33m$\x1B[0m ");
 
 }
+/**
+ * listenread - get char the client terminal & call fn until char is '\n'
+ *@buffer: pointer
+ * Return: 1 is successful, -1 if error.
+ */
+int listenread(char *buffer)
+{
+	size_t bufsize = SIZEBUFFER;
+	ssize_t c;
+
+	fflush(stdin);
+	c = getline(&buffer, &bufsize, stdin);
+	if (c == EOF)
+		return (-1);
+	else if (_strcmp(buffer, "exit") == 10)
+		return (-1);
+	return (0);
+}
