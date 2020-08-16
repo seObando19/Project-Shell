3993026dbc03c582321caecff1860ab57876fc3c
hfog2066 <hfog2000@yahoo.com>
2020-08-16 15:11:27 -0500

    created getargs function

--- shellfunc1.c
+++ shellfunc1.c
@@ -28,3 +28,54 @@ int listenread(char *buffer)
 		return (-1);
 	return (0);
 }
+/**
+ * get_simple_args - this fn splits the string when space char is found
+ *@argc: argument counter
+ *@argv: where the arguments are contained
+ *@args: arguments
+ *@_path: path
+ *@ev: pointer that contain enviroment vars
+ * Return: status of execution
+ */
+int get_simple_args(int argc, char **argv, char *args, char **_path, char **ev)
+{
+	char delim[] = " \n";
+	char **options;
+	char *argx;
+	int j = 0;
+	int args_max = 255, status_pid = 0;
+	(void)_path;
+
+	if (argc == 1)
+	{
+		if ((int)args[0] == 10)
+			free(args);
+		else
+		{
+			options = malloc(sizeof(char *) * args_max);
+			if (options == NULL)
+			{
+				free(options);
+				exit(111);
+			}
+			argx = strtok(args, delim);
+			while (argx != NULL)
+			{options[j] = argx;
+				argx = strtok(NULL, delim);
+				j++; }
+			options[j] = NULL;
+			if (_strcmp(options[0], "env") == 0 && options[1] == NULL)
+				printenv(ev);
+			else if (options[0] != NULL && _strlen(options[0]) > 0)
+				status_pid = pid_launch(options, args, _path);
+			free(args);
+			free(options);
+		}
+	}
+	else if (argc >= 2)
+	{
+		args_nval(argv);
+		status_pid = pid_launch(argv, NULL, _path);
+	}
+	return (status_pid);
+}
