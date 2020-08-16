c1886940711c0d4a3c7fe939d5e8e43b2165ef4f
hfog2066 <hfog2000@yahoo.com>
2020-08-16 12:42:02 -0500

    holberton.h prototypes

---
+++ holberton.h
@@ -0,0 +1,34 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <assert.h>
+#include <errno.h>
+#define SIZEBUFFER 1024
+
+char *str_concat(char *s1, char *s2);
+char *get_value_env(char **envp, char *variable);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+void intHandler(int i);
+void prompt(void);
+int listenread(char *);
+int get_simple_args(int, char **, char *, char **, char **);
+int pid_launch(char **, char *, char **);
+void _error(void);
+long int _strlen(char *);
+int args_nval(char **);
+char *getpath(char **);
+char **setpathparams(char *);
+int appenddir(char *);
+int printenv(char **);
+#endif
