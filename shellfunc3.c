e8bd0a5fde3a4654c94b5df1a1221dbc6fbc9fce
seObando19 <sebastiano-10@hotmail.com>
2020-08-16 20:53:42 +0000

    Added function appenddir

--- shellfunc3.c
+++ shellfunc3.c
@@ -17,3 +17,24 @@ int printenv(char **env)
 	}
 	return (0);
 }
+
+/**
+ * appenddir - appends the "." and "/" of the commands in env path
+ *@cmd: command
+ * Return: 0 or 1
+ */
+int appenddir(char *cmd)
+{
+	int i = 0;
+
+	while (cmd[i] != '\0')
+		i++;
+	if (i > 2)
+	{
+		if (cmd[0] == '.' || cmd[0] == '/')
+			return (0);
+	}
+	return (1);
+}
+
+
