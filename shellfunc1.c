ad17dc7c849dcde78f646217395c60864f41af07
hfog2066 <hfog2000@yahoo.com>
2020-08-18 22:29:43 -0500

    error codes

--- shellfunc1.c
+++ shellfunc1.c
@@ -85,11 +85,12 @@ int get_simple_args(int argc, char **argv, char *args, char **_path, char **ev)
  */
 void _error(void)
 {
-	/*write(STDERR_FILENO, __FILE__, _strlen(__FILE__));*/ 
-	write(STDERR_FILENO, "sh", 2);
+	write(STDERR_FILENO, __FILE__, _strlen(__FILE__));
+	write(STDERR_FILENO, ": ", 2);
 	write(STDERR_FILENO, ": No such file or directory",
 	 _strlen("/: No such file or directory"));
-	write(STDERR_FILENO, "\n", _strlen("\n"));
+	/* write(STDERR_FILENO, "\n", _strlen("\n"));*/
+
 }
 /**
  * pid_launch - make a fork to create child process who execute the command
